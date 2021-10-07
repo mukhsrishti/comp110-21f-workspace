2ade8d845956b6daa62c8f67f40c9d09ddc48f5a
srishtii <89877534+mukhsrishti@users.noreply.github.com>
2021-10-06 22:32:00 -0400

    Update happy_trees.py

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,9 @@
 """Drawing forests in a loop."""
-
-__author__ = "123456789"
+__author__ = "730402368"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+def main():
+    greeting: str = input("Enter number of rows: ")
+    print (f"\U0001F332")
